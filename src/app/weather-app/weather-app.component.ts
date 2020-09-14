bf40239154b8e6aa2c97ed74deac63dab5ec985c
rounakbhowmick <rounakchakda@gmail.com>
2020-09-14 09:37:12 +0530

    feels like feature added

--- src/app/weather-app/weather-app.component.ts
+++ src/app/weather-app/weather-app.component.ts
@@ -11,7 +11,6 @@ export class WeatherAppComponent implements OnInit {
   temp: string;
   date: Date;
   feels_like: string;
-  isDay: boolean;
   city = '';
   wind: number;
   humidity: string;
@@ -19,10 +18,13 @@ export class WeatherAppComponent implements OnInit {
   iconcode: number;
   iconUrl: string;
   error: String;
+  obj: object;
   fetchWeather(city: string) {
     this.weatherservice.getWeather(city).subscribe(
       (response) => {
         this.setWeatherData(response);
+        var obj = { City: response.name };
+        console.log(obj[0]);
       },
       (error) => {
         this.error = error;
@@ -36,11 +38,9 @@ export class WeatherAppComponent implements OnInit {
   }
   setWeatherData(data) {
     this.temp = (data.main.temp - 273.15).toFixed(0);
-    // let sunsetTime = new Date(data.sys.sunset * 1000);
-    //console.log(this.temp);
     this.date = new Date();
-    // this.feels_like = (data.main.feels_like - 273.15).toFixed(0);
-    // this.isDay = this.date.getTime() < sunsetTime.getTime();
+    this.feels_like = (data.main.feels_like - 273.15).toFixed(0);
+
     this.wind = Math.round(Number(data.wind.speed) * 1.609);
     this.humidity = data.main.humidity;
     this.city = data.name;
@@ -48,7 +48,6 @@ export class WeatherAppComponent implements OnInit {
     this.iconcode = data.weather[0].icon;
     //console.log(this.iconcode);
     this.iconUrl = `http://openweathermap.org/img/w/${this.iconcode}.png`;
-    console.log(this.isDay);
   }
   refresh() {
     this.temp = '';
